5acae2f09b335e8ba263d0f88491ee217db86a65
Rahul Deshwal <mailboxrahul1@gmail.com>
2025-10-22 00:59:47 +0530

    change base path

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/ui/drug-info-app/',
+  base: '/drug-info',
   server: {
     host: true,
     strictPort: true,
